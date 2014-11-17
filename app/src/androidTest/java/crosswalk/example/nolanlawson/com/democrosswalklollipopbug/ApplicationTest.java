31c69db4108e49970412b935315ffa640227a634
Nolan Lawson <nolan.lawson@gmail.com>
2014-11-17 16:10:06 -0500

    initial commit

---
+++ app/src/androidTest/java/crosswalk/example/nolanlawson/com/democrosswalklollipopbug/ApplicationTest.java
@@ -0,0 +1,13 @@
+package crosswalk.example.nolanlawson.com.democrosswalklollipopbug;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
